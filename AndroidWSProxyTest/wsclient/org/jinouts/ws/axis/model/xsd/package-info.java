557344f5427c5a552dfc8de3d5103f53d3de91a5
Petr Havlena <havlenapetr@gmail.com>
2014-11-23 00:32:54 +0100

    rm AndroidWSProxyTest

--- AndroidWSProxyTest/wsclient/org/jinouts/ws/axis/model/xsd/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.jinouts.xml.bind.annotation.XmlSchema(namespace = "http://model.axis.ws.jinouts.org/xsd", elementFormDefault = org.jinouts.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.jinouts.ws.axis.model.xsd;
--- AndroidWSProxyTest/wsclient/org/jinouts/ws/axis/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.jinouts.xml.bind.annotation.XmlSchema(namespace = "http://axis.ws.jinouts.org", elementFormDefault = org.jinouts.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.jinouts.ws.axis;
